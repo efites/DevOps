a6563d246fea3441c3eb33a061d4f7c9fc84cdbd
Mny <artem_molkov@senla.eu>
2025-03-06 12:22:22 +0300

    👑 check tests

--- src/utils/sum.test.ts
+++ src/utils/sum.test.ts
@@ -3,7 +3,7 @@ import {expect, it} from 'vitest'
 import {sum} from './sum'
 
 it('adds 1 + 2 to equal 3', () => {
-	expect(sum(1, 2)).toBe(4)
+	expect(sum(1, 2)).toBe(3)
 })
 
 it('adds 0 + 0 to equal 0', () => {
